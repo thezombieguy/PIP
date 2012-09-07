878b3c4fe61ead51f8a6bb4432431cc48e25748e
thezombieguy <bryan@bryantrudel.com>
2012-09-06 23:47:06 -0700

    added a fetch command for the view to retrieve the html for use in a php var

--- system/view.php
+++ system/view.php
@@ -23,6 +23,18 @@ class View {
 		require($this->template);
 		echo ob_get_clean();
 	}
+	
+	public function fetch()
+  {
+    extract($this->pageVars);
+
+    ob_start();
+    require($this->template);
+    $contents = ob_get_contents();
+    ob_end_clean();
+    return $contents;
+  }
+
     
 }
 
