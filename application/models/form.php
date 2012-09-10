73129a2800f3592c7dfd971cfaf5721392ead948
thezombieguy <bryan@bryantrudel.com>
2012-09-09 22:35:20 -0700

    rebuilding form api. Needs to be moved to a helper not a model

--- application/models/form.php
+++ application/models/form.php
@@ -1,40 +1,85 @@
 <?php
 
-class Form
+class Form extends Model
 {
 
-  private static $instance = NULL;
-
-  // get Singleton instance of Form class
-
-  public static function getInstance()
+  private function setAttributes($attributes = array())
   {
-    if (self::$instance === NULL)
-    {
-      self::$instance = new self;
+    
+    $attr = array();
+    
+    if(!empty($attributes)){ 
+      foreach($attributes as $key => $val){
+        $attr[] = '"'.$key.'"="'.$val.'"';
+      }
     }
-    return self::$instance;
-
+    
+    return implode(' ', $attr);
   }
-
-
-  public static function build($form)
+  /*
+   * if we use this, we can detect a session form build error, rebuild the form, and if ERROR = true
+   * we can rebuild the form with the session data vars already filled in. also, we could highlight the error areas.
+   */
+  public function build($form)
   {
-    //print_r($form);
+
     $html = '';
-    foreach($form as $element => $attributes)
+    
+    $html .= $this->open($form);
+    $form['#elements'][] = array('type' => 'hidden', 'name' => 'form_id', 'value' => $form['#form_id']);
+    foreach($form['#elements'] as $element => $attributes)
     {
       $prefix = isset($attributes['#prefix']) ? $attributes['#prefix'] : '';
       $suffix = isset($attributes['#suffix']) ? $attributes['#suffix'] : '';
       switch($attributes['type'])
       {
-        case "textarea":
+        case 'markup':
+          $html .= $attributes['markup'];
           break;
+        case "textarea":
+          $html .= $prefix . $this->textarea($attributes) . $suffix;
+          break; 
+        case "label":
+          $html .= $prefix . $this->label($attributes) . $suffix;
+          break;
+        case "select":
+          $html .= $prefix . $this->select($attributes) . $suffix;
+          break;
+        case "input":
+        case "hidden":
         default:
-          $html .= $prefix . '<input type="'.$attributes['type'].'" name="'.$attributes['name'].'" value="'.$attributes['value'].'" />' . $suffix;
+          $html .= $prefix . $this->input($attributes) . $suffix;
           break;
       }
     }
+    $html .= $this->close();
+    return $html;
+  }
+  
+  private function input($attributes)
+  {
+    return '<input type="'.$attributes['type'].'" name="'.$attributes['name'].'" value="'.$attributes['value'].'" '.$this->setAttributes($attributes['#attributes']).' />';
+  }
+  
+  private function textarea($attributes)
+  {
+    return '<'.$attributes['type'].' name="'.$attributes['name'].'" value="'.$attributes['value'].'" rows="'.$attributes['rows'].'" cols="'.$attributes['cols'].'" />';
+  }
+  
+  private function label($attributes)
+  {
+    return '<' . $attributes['type'] . ' for="' . $attributes['for'] . '" >' . $attributes['title'] . '</label>';
+  }
+  
+  private function select($attributes)
+  {
+    $html = '';
+    
+    $html = '<select name="' . $attributes['name'] . '" ' .$this->setAttributes($attributes['#attributes']) . '">';
+    foreach($attributes['options'] as $option => $value){
+      $html .= '<option value="'.$option.'">'.$value.'</option>';
+    }
+    $html .= '</select>';
     
     return $html;
   }
@@ -42,80 +87,11 @@ class Form
 
   // render <form> opening tag
 
-  public static function open(array $attributes)
+  public function open($form)
   {             
-    $html = '<form';
-
-    if (!empty($attributes))
-      {
-      foreach ($attributes as $attribute => $value)
-      {
-        if (in_array($attribute, array('action', 'method', 'id', 'class', 'enctype')) and !empty($value))
-        {
-          // assign default value to 'method' attribute
-          if ($attribute === 'method' and ($value !== 'post' or $value !== 'get'))
-          {
-            $value = 'post';
-          }
-          $html .= ' ' . $attribute . '="' . $value . '"';
-        }
-      }
-    }
-
-    return $html . '>';
-
+    $html = '<form name="'.$form['name'].'" method="'.$form['method'].'" action="'.$form['action'].'" >';
+    return $html;
   }
-
-
-
-  // render <input> tag
-
-  public static function input(array $attributes)
-  {             
-    $html = '<input';
-
-    if (!empty($attributes))
-    {
-      foreach ($attributes as $attribute => $value)
-      {
-        if (in_array($attribute, array('type', 'id', 'class', 'name', 'value')) and !empty($value))
-        {
-          $html .= ' ' . $attribute . '="' . $value . '"';
-        }
-      }
-    }
-    return $html . '>';
-
-  }
-
-
-
-  // render <textarea> tag
-
-  public static function textarea(array $attributes)
-  {             
-    $html = '<textarea';
-    $content = '';
-
-    if (!empty($attributes))
-    {
-      foreach ($attributes as $attribute => $value)
-      {
-        if (in_array($attribute, array('rows', 'cols', 'id', 'class', 'name', 'value')) and !empty($value))
-        {
-          if ($attribute === 'value')
-          {
-            $content = $value;
-            continue;
-          }
-          $html .= ' ' . $attribute . '="' . $value . '"';
-        }
-      }
-    }
-    return $html . '>' . $content . '</textarea>';
-
-  }
-
 
 
   // render </form> closing tag
@@ -127,5 +103,3 @@ class Form
 
 }// End Form class
 
-
-//Read more at http://www.devshed.com/c/a/PHP/Finishing-a-Form-Helper-Class-for-an-MVCbased-Framework-in-PHP-5/3/#SEYmZJmprYq3Yo00.99
