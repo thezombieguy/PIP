e2d6bfe31666a77fd083c789c76ebb75337cdd7f
thezombieguy <bryan@bryantrudel.com>
2012-09-15 09:45:12 -0700

    updated base url

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-$config['base_url'] = ''; // Base URL including trailing slash (e.g. http://localhost/)
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/'; // Base URL including trailing slash (e.g. http://localhost/)
 
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
@@ -9,5 +9,7 @@ $config['db_host'] = ''; // Database host (e.g. localhost)
 $config['db_name'] = ''; // Database name
 $config['db_username'] = ''; // Database username
 $config['db_password'] = ''; // Database password
+
+
 
 ?>
