51c8e85b1ae77bdef08f7583f35aef714d2d2998
thezombieguy <bryan@bryantrudel.com>
2012-09-14 21:53:08 -0700

    added some notes for help

--- application/models/http_model.php
+++ application/models/http_model.php
@@ -3,6 +3,16 @@
   class http_model extends Model
   {
     
+    /*
+    $context = array(
+      'content' => $data,
+      'user' => 'guest',
+      'pass' => 'guest',
+      'header' => array(
+        'Content-type: text/xml',//can have as many header values as you need.
+      )
+    );
+    */
     public function setContext($context)
     {
       $stream_options = array();
@@ -12,18 +22,22 @@
       {
         
         $http['method'] = !empty($context['method']) ? $context['method'] : 'POST';
+        
+        //lets get some headers
         if(!empty($context['header'])){
           $http['header'] = '';
           foreach($context['header'] as $header){
             $http['header'] .= $header. "\r\n";
           }
         }
+        //if user/pass is set, we automatically create an authorization header request
         $http['header'] .= (!empty($context['user']) && !empty($context['pass'])) ?'Authorization: Basic ' . base64_encode($context['user'].':'.$context['pass']). "\r\n" : '';
         $http['content'] = !empty($context['content']) ? $context['content'] : '';
         $stream_options['http'] = $http;
         
         return $stream_options;  
       }
+      return;
     }
     
     public function request($url, $data = array())
