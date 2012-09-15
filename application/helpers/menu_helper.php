ae3a7d04ab5b83089210e7fbd5dc4796311cefe4
thezombieguy <bryan@bryantrudel.com>
2012-09-15 11:50:14 -0700

    moving child elements of menu helper into proper position

--- application/helpers/menu_helper.php
+++ application/helpers/menu_helper.php
@@ -4,20 +4,16 @@ class Menu_helper
 {
   private $load;
   
-  public function __construct()
-  {
-    $load = new Load();
-  }
-  
   public function build($items)
   {
     $nav = '<ul>';
     
     foreach($items as $item){
-      if(isset($item['children'])){
-        $nav .= $this->build($item['children']);
-      }
-      $nav .= '<li class="nav"><a href="'.$item['url'].'">'.$item['title'].'</a></li>';
+      
+      $nav .= '<li class="nav"><a href="'.$item['url'].'">'.$item['title'].'</a>';
+      $nav .= isset($item['children']) ? $this->build($item['children']) : NULL;
+      $nav .= '</li>';
+      
     }
     $nav .= '</ul>';
     
