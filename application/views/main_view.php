fa8b6bbdb04f4d80a511b0c18dd4237291434543
thezombieguy <bryan@bryantrudel.com>
2012-09-15 12:15:04 -0700

    testing main view

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -2,8 +2,7 @@
 	
     <div id="content">
         
-        <h1>Welcome to PIP</h1>
-        <p>To get started please read the documentation at <a href="http://pip.dev7studios.com/">http://pip.dev7studios.com</a>.</p>
+        <?php print $content; ?>
         
     </div>
 
