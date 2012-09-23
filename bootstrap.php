1d68546e963fde1c86a1096fb7e6c4ee58cd67d3
thezombieguy <bryan@bryantrudel.com>
2012-09-23 12:40:31 -0700

    trying to move reusable loader data to a bootstrap.

---
+++ bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+// Defines
+define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+define('APP_DIR', ROOT_DIR .'application/');
+
+require(APP_DIR .'config/config.php');
+require(ROOT_DIR .'system/autoload.php');
+
+// Define base URL
+global $config;
+define('BASE_URL', $config['base_url']);
+
+//Start the Session
+session_start();
+//autoload
+Autoload::getInstance();
--- index.php
+++ index.php
@@ -3,22 +3,7 @@
  * PIP v0.5.3
  */
 
-// Defines
-define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
-define('APP_DIR', ROOT_DIR .'application/');
-
-require(APP_DIR .'config/config.php');
-require(ROOT_DIR .'system/autoload.php');
-
-
-// Define base URL
-global $config;
-define('BASE_URL', $config['base_url']);
-
-//Start the Session
-session_start();
-//autoload
-Autoload::getInstance();
+include('bootstrap.php');
 //initialize
 $pip = new Pip();
 
