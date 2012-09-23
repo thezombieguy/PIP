076184f41f4424e284c8f2cf7d8d77c7779d4449
thezombieguy <bryan@bryantrudel.com>
2012-09-23 12:38:11 -0700

    adding a menu storage for reusable menu items

---
+++ application/config/menus.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$menu['main_menu'] = 
+      array(
+        array(
+          'title' => 'Home',
+          'url' => '',
+        ),
+        array(
+          'title' => 'Test Index',
+          'url' => 'test',
+        ),
+      );
+
+$menu['test_menu'] = array(
+        array(
+          'title' => 'Home',
+          'url' => '',
+        ),
+        array(
+          'title' => 'Test Index',
+          'url' => 'test',
+          'children' => array(
+            array(
+              'title' => 'Test Form',
+              'url' => 'test/form',
+            ),
+            array(
+              'title' => 'Test Cache',
+              'url' => 'test/cache',
+            ),
+            array(
+              'title' => 'Test Menu',
+              'url' => 'test/menu',
+            ),
+            array(
+              'title' => 'Test xml',
+              'url' => 'test/xml',
+            ),
+          ),
+        ),
+      );
