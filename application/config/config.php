9db3e826ab7dc5d7e5fc5e58e1f3bcbffd9e0d69
thezombieguy <bryan@bryantrudel.com>
2013-04-14 01:01:58 -0700

    tweaking and fixing a couple of minor issues

--- application/config/config.php
+++ application/config/config.php
@@ -4,8 +4,6 @@
 define('SITE_NAME', 'www.example.com');
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
 
-//$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/'; // Base URL including trailing slash (e.g. http://localhost/)
-
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
