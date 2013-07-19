81a4b2ae5f81b03647361fc64e3c586f4ad46618
thezombieguy <bryan@bryantrudel.com>
2013-07-18 21:26:45 -0700

    cleaning up config

--- application/config/config.php
+++ application/config/config.php
@@ -3,12 +3,13 @@
 //some constants
 define('SITE_NAME', 'www.example.com');
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].'/');
+define('ENVIRONMENT', 'dev');//dev, stg, uat, prd
 
 $config['default_controller'] = 'main'; // Default controller to load
 $config['error_controller'] = 'error'; // Controller used for errors (e.g. 404, 500 etc)
 
 $config['db_host'] = 'localhost'; // Database host (e.g. localhost)
-$config['db_name'] = 'portfolio'; // Database name
+$config['db_name'] = ''; // Database name
 $config['db_username'] = ''; // Database username
 $config['db_password'] = ''; // Database password
 
